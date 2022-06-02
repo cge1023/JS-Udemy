6bdcbf175761c00982f0803ffaaa5232b25627d5
cge1023 <ge931023@gmail.com>
2022-06-02 18:27:53 +0900

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+//Values and Variables
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Kaylyn");
+console.log("Kaylyn");
+console.log("Kaylyn");
+
+let firstName = "Kaylyn";
+let $function = 27;
+
+//Variable names cannot start with a number.
+//Variable names can only contain numbers, letters, underscores, or the dollar sign.
+
+let person = "Kaylyn";
+
+//Variables that are all in uppercase are reserved for
+//constants that we know will never change.
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log("myFirstJob");
