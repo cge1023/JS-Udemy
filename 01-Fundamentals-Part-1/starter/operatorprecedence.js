14d212dd735f0a385dd85cbf96dcf9b44466f523
cge1023 <ge931023@gmail.com>
2022-06-07 15:25:31 +0900

    Operator Precedence

---
+++ 01-Fundamentals-Part-1/starter/operatorprecedence.js
@@ -0,0 +1,14 @@
+//Operator Precedence
+
+const now = 2022;
+const ageKaylyn = now - 1993;
+const ageSarah = now - 2005;
+
+console.log(now - 1993 > now - 2005);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const averageAge = (ageKaylyn + ageSarah) / 2;
+console.log(ageKaylyn, ageSarah, averageAge);
