df59f0face9d91915b96dd815ef4fe56368f1dee
cge1023 <ge931023@gmail.com>
2022-06-18 23:29:40 +0900

    The switch Statement / Statements and Expressions

---
+++ 01-Fundamentals-Part-1/starter/statementsandexpressions.js
@@ -0,0 +1,12 @@
+//Statements and Expressions
+
+//expression is a piece of code that produces a value
+3 + 4;
+1991;
+true && false && !false;
+
+//statement is like a bigger piece of code that is executed and which does not produce a value on itself
+//statements are like full sentences that translate our actions
+if (23 > 10) {
+  const str = "23 is bigger";
+}
