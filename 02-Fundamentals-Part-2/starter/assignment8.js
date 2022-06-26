63d6a08fb53be0250693a90d4ad5997fda798d2c
cge1023 <ge931023@gmail.com>
2022-06-26 23:28:32 +0900

    assignment 8

--- 02-Fundamentals-Part-2/starter/assignment8.js
+++ 02-Fundamentals-Part-2/starter/assignment8.js
@@ -9,3 +9,5 @@ const myCountry = {
 console.log(
   `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`
 );
+myCountry.population += 2;
+myCountry["population"] -= 2;
